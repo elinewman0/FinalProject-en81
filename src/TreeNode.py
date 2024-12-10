294f0a7388b228e69796f718fb5cd02d4333ade4
elinewman0 <elinewman0@icloud.com>
2024-12-09 23:14:34 -0500

    added docstring for TreeNode class

--- src/TreeNode.py
+++ src/TreeNode.py
@@ -1,4 +1,18 @@
 class TreeNode:
+    """
+    Represents a node in the decision tree.
+
+    Attributes:
+        attribute (str or None): The attribute name this node splits on. If None,
+            the node is a leaf.
+        isLeaf (bool): Indicates whether this node is a leaf node. Leaf nodes do not
+            split further.
+        prediction (str or None): The class prediction if this is a leaf node.
+            If None and isLeaf is True, a default or majority class should be
+            assigned.
+        children (dict): A mapping from attribute values to subsequent TreeNode
+            objects. Used only if isLeaf is False.
+    """
     def __init__(self, attribute=None, isLeaf=False, prediction=None):
         self.attribute = attribute
         self.isLeaf = isLeaf
